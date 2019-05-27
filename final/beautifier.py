cffd5e196e3aa56bbcdf985d89af4bebd063aeae
Julia <46975163+holajulia@users.noreply.github.com>
2019-05-27 22:45:51 +0300

    Delete beautifier.py

--- final/beautifier.py
+++
@@ -1,8 +0,0 @@
-with open('dict_new.txt', 'r') as f:
-    with open('dict.txt', 'w') as new_dict: 
-        words = f.read()
-        words = words.split()
-        words = list(set(words))
-        words.sort()
-        for word in words:
-            new_dict.write(word + '\n')
