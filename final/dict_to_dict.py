f5dba7649187efb3f43380c9fd810546d0c0affc
Julia <46975163+holajulia@users.noreply.github.com>
2019-05-28 22:08:14 +0300

    Update dict_to_dict.py

--- final/dict_to_dict.py
+++ final/dict_to_dict.py
@@ -1,7 +1,7 @@
 with open('dict.opcorpora.txt', 'r', encoding='utf-8') as f:
     count = 0
-    nth = 1000
-    group = 20
+    nth = 100
+    group = 40
     with open('dict_new.txt', 'w', encoding='utf-8') as new_dict:
         for line in f:
             if '\t' in line and (count // group % nth) == 0:
